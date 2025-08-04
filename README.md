28ac33aaac0728eb60b029fc1be0101981f0a156
唐加彬 <me@tangjiabin.cn>
2025-08-04 15:12:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# tin-config-load
+
+一个简单的 Rust 配置加载库，基于 [config](https://docs.rs/config/) crate，支持从文件和环境变量加载配置。
---
+++ src/config.rs
@@ -0,0 +1,25 @@
+pub struct ConfigLoader;
+
+impl ConfigLoader {
+    pub fn load<T: serde::de::DeserializeOwned>() -> Result<T, config::ConfigError> {
+        let s = config::Config::builder()
+            .add_source(config::File::with_name("./config/default"))
+            .add_source(config::Environment::with_prefix("APP"))
+            .build()?;
+        s.try_deserialize()
+    }
+
+    pub fn file<T: serde::de::DeserializeOwned>(path: &str) -> Result<T, config::ConfigError> {
+        let s = config::Config::builder()
+            .add_source(config::File::with_name(path))
+            .build()?;
+        s.try_deserialize()
+    }
+
+    pub fn env<T: serde::de::DeserializeOwned>(prefix: &str) -> Result<T, config::ConfigError> {
+        let s = config::Config::builder()
+            .add_source(config::Environment::with_prefix(prefix))
+            .build()?;
+        s.try_deserialize()
+    }
+}
